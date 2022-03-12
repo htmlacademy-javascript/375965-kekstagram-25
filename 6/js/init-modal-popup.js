57c1006d0fbde6214054b3117f2d5bdb5adc535f
Keks <keks@htmlacademy.ru>
2022-03-12 19:38:52 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/init-modal-popup.js
@@ -0,0 +1,56 @@
+import { posts } from './data.js';
+
+const initModalPopup = () => {
+  const picturesContainer = document.querySelector('.pictures');
+  const fullPhoto = document.querySelector('.big-picture');
+  const pictureCloseButton = document.querySelector('.big-picture__cancel');
+  const commentsTemplate = document.querySelector('#comments').content.querySelector('.social__comment');
+
+  const commentsContainer = document.querySelector('.social__comments');
+  const bigPictureImg = document.querySelector('.big-picture__img img');
+  const commentsCount = document.querySelector('.comments-count');
+  const likesCount = document.querySelector('.likes-count');
+  const socialCaption = document.querySelector('.social__caption');
+  const commentCountArea = document.querySelector('.social__comment-count');
+  const commentsLoader = document.querySelector('.comments-loader');
+
+  const similarCommentsFragment = document.createDocumentFragment();
+
+  picturesContainer.addEventListener('click', (event) => {
+    const postId = event.target.closest('a.picture').dataset.postId - 1;
+    const currentPost = posts[postId];
+    fullPhoto.classList.remove('hidden');
+    document.body.classList.add('modal-open');
+    commentCountArea.classList.add('hidden');
+    commentsLoader.classList.add('hidden');
+    bigPictureImg.src = currentPost.url;
+    commentsCount.textContent = currentPost.comments;
+    likesCount.textContent = currentPost.likes;
+    socialCaption.textContent = currentPost.description;
+    commentsContainer.innerHTML = '';
+
+    posts[postId].comments.forEach(({ avatar, message, name}) => {
+      const commentsItem = commentsTemplate.cloneNode(true);
+      const commentImg = commentsItem.querySelector('.social__picture');
+      commentImg.src = avatar;
+      commentImg.alt = name;
+      commentsItem.querySelector('.social__text').textContent = message;
+      similarCommentsFragment.appendChild(commentsItem);
+    });
+    commentsContainer.appendChild(similarCommentsFragment);
+  });
+
+  pictureCloseButton.addEventListener('click', () => {
+    fullPhoto.classList.add('hidden');
+    document.body.classList.remove('modal-open');
+  });
+
+  document.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape') {
+      fullPhoto.classList.add('hidden');
+      document.body.classList.remove('modal-open');
+    }
+  });
+};
+
+export { initModalPopup };
