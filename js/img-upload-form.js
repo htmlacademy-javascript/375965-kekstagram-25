b14206661e965fcdc3e9b821737af00f82ed39c1
Abay <temirbolatov17@gmail.com>
2022-03-18 01:10:59 +0600

    исправил валидацию и ошибку при закрытии формы

--- js/img-upload-form.js
+++ js/img-upload-form.js
@@ -11,9 +11,20 @@ const re = /^#[A-Za-zA-Яа-яЕё0-9]{1,19}$/;
 
 const pristine = new Pristine(postUploadForm);
 
-const openOverlay = (callback) => {
+let isHashTagFieldFocused = false;
 
-  callback.addEventListener('change', () => {
+hashTagsField.addEventListener('focus', () => { isHashTagFieldFocused = true; });
+hashTagsField.addEventListener('focusout', () => { isHashTagFieldFocused = false; });
+
+const onCloseOverlay = () => {
+  postUploadForm.reset();
+  uploadOverlay.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  picturesContainer.addEventListener('click', onClickPictures);
+};
+
+const imgDownloadOverlay = () => {
+  uploadFileItem.addEventListener('change', () => {
     uploadOverlay.classList.remove('hidden');
     document.body.classList.add('modal-open');
     picturesContainer.removeEventListener('click', onClickPictures);
@@ -23,36 +34,29 @@ const openOverlay = (callback) => {
   });
 
   uploadCancel.addEventListener('click', () => {
-    uploadOverlay.classList.add('hidden');
-    document.body.classList.remove('modal-open');
-    picturesContainer.addEventListener('click', onClickPictures);
+    onCloseOverlay();
   });
 
   document.addEventListener('keydown', (evt) => {
-    if (evt.key === 'Escape') {
-      uploadOverlay.classList.add('hidden');
-      document.body.classList.remove('modal-open');
+    if (evt.key === 'Escape' && !isHashTagFieldFocused) {
+      onCloseOverlay();
     }
-    picturesContainer.addEventListener('click', onClickPictures);
   });
-
 };
 
 function validateHashtags (value) {
-  const arr = value.toString().split(' ');
-  // re.test(arr[0]);
-  // if (arr.length > 1) {
-  //   arr.forEach((element) => {
-  //     const result = re.test(arr[element]);
-  //     return result;
-  //   });
-  // }
+  const hashTagList = value.toString().split(' ');
 
-  for (let i = 0; i <= arr.length; i++) {
-    if (re.test(arr[i]) === true) {
-      return true;
+  for (let i = 0; i < hashTagList.length; i++) {
+    if (re.test(hashTagList[i]) === false) {
+      return false;
+    } else if (hashTagList[i].length > 20) {
+      return false;
+    } else if (hashTagList.length > 5) {
+      return false;
     }
   }
+  return true;
 }
 
 function validateComment (value) {
@@ -62,18 +66,13 @@ function validateComment (value) {
 pristine.addValidator(hashTagsField, validateHashtags, 'Ошибка');
 pristine.addValidator(commentField, validateComment, 'Ошибка');
 
-
 postUploadForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  pristine.validate();
-
   const isValid = pristine.validate();
   if (isValid) {
     return true;
   } else {
-    // console.log('not valid');
+    evt.preventDefault();
   }
 });
 
-openOverlay(uploadFileItem);
-
+export { imgDownloadOverlay };
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { renderPictures } from './render-pictures.js';
 import { initModalPopup } from './init-modal-popup.js';
-import './img-upload-form.js';
+import { imgDownloadOverlay } from './img-upload-form.js';
 
 renderPictures();
 initModalPopup();
+imgDownloadOverlay();
